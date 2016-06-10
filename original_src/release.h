6172d7557a9ba1bae3531f908df9e513c3f60cbe
snackerh <whanlee4@naver.com>
2016-06-10 14:22:25 +0900

    NOWAY

--- original_src/release.h
+++ original_src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "3b89f746"
-#define REDIS_GIT_DIRTY "13"
-#define REDIS_BUILD_ID "readys-gateway-1464943873"
+#define REDIS_GIT_SHA1 "a7a68e72"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "readys-gateway-1465534790"
